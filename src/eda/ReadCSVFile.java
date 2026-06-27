e410a8bdc12064f61601bc68c4d7bdf5f1d1d6de
YASSINE B13 <yagamiilight1986@gmail.com>
2026-06-27 22:37:18 +0100

    Update Test.java & ReadCSVFile.java

--- src/eda/ReadCSVFile.java
+++ src/eda/ReadCSVFile.java
@@ -63,7 +63,6 @@ public class ReadCSVFile {
                  integer = false;
              }
 
-
              try {
                  Double.parseDouble(value);
              } catch (NumberFormatException e) {
